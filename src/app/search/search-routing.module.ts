0d0c44ed3901fc57706f5572aef41b6b8ddae4f8
André Neves <andreneves3@gmail.com>
2022-04-26 00:27:36 -0300

    feat(github search): configure search module

--- src/app/search/search-routing.module.ts
+++ src/app/search/search-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GithubSearchPageComponent } from './presenter/pages/github-search/github-search-page/github-search-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: GithubSearchPageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class SearchRoutingModule { }
+export class SearchRoutingModule {}
--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -2,13 +2,35 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SearchRoutingModule } from './search-routing.module';
-
+import { SearchRepository } from './domain/repositories/search-repository.interface';
+import { GithubSearchPageComponent } from './presenter/pages/github-search/github-search-page/github-search-page.component';
+import { GithubSearchController } from './presenter/pages/github-search/github-search.controller';
+import { SearchRepositoryService } from './infra/repositories/search-repository.service';
+import { GithubSearchSearchBoxComponent } from './presenter/pages/github-search/components/organisms/github-search-search-box/github-search-search-box.component';
+import { GithubSearchSearchResultsComponent } from './presenter/pages/github-search/components/organisms/github-search-search-results/github-search-search-results.component';
+import { FormsModule } from '@angular/forms';
+import { GithubSearchUsecase } from './domain/usecases/github-search.usecase';
+import { GithubSearchDatasource } from './infra/datasources/github-search-datasource.interface';
+import { GithubSearchDatasourceService } from './external/datasources/github-search-datasource.service';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    SearchRoutingModule
-  ]
+  declarations: [
+    GithubSearchPageComponent,
+    GithubSearchSearchBoxComponent,
+    GithubSearchSearchResultsComponent,
+  ],
+  providers: [
+    GithubSearchController,
+    GithubSearchUsecase,
+    {
+      provide: SearchRepository,
+      useClass: SearchRepositoryService,
+    },
+    {
+      provide: GithubSearchDatasource,
+      useClass: GithubSearchDatasourceService,
+    },
+  ],
+  imports: [CommonModule, FormsModule, SearchRoutingModule],
 })
-export class SearchModule { }
+export class SearchModule {}
