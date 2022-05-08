97dee22c40735c93f0af72bcb9a4c74cdab050b7
André Neves <andreneves3@gmail.com>
2022-05-08 10:30:16 -0300

    test(basic): adjustments on github search state files

--- src/app/search/presenter/pages/github-search/states/github-search-filter.state.spec.ts
+++ src/app/search/presenter/pages/github-search/states/github-search-filter.state.spec.ts
@@ -4,4 +4,25 @@ describe('GithubSearchFilterState', () => {
   it('should create an instance', () => {
     expect(GithubSearchFilterState.default()).toBeTruthy();
   });
+
+  it('should create a instance with custom searchTerm', () => {
+    const searchTerm = 'my custom search';
+    const state = new GithubSearchFilterState(searchTerm);
+
+    expect(state).toEqual(
+      jasmine.objectContaining({
+        searchTerm,
+      })
+    );
+  });
+
+  it('should create a default instance with empty searchTerm', () => {
+    const state = GithubSearchFilterState.default();
+
+    expect(state).toEqual(
+      jasmine.objectContaining({
+        searchTerm: '',
+      })
+    );
+  });
 });
--- src/app/search/presenter/pages/github-search/states/github-search-view.state.spec.ts
+++ src/app/search/presenter/pages/github-search/states/github-search-view.state.spec.ts
@@ -4,4 +4,25 @@ describe('GithubSearchViewState', () => {
   it('should create an instance', () => {
     expect(GithubSearchViewState.default()).toBeTruthy();
   });
+
+  it('should create a instance with custom page', () => {
+    const page = 10;
+    const state = new GithubSearchViewState(page);
+
+    expect(state).toEqual(
+      jasmine.objectContaining({
+        page,
+      })
+    );
+  });
+
+  it('should create a default instance that shows first page', () => {
+    const state = GithubSearchViewState.default();
+
+    expect(state).toEqual(
+      jasmine.objectContaining({
+        page: 1,
+      })
+    );
+  });
 });
