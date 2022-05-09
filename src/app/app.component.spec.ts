7b97daf2bcf13b55f18435bafce28ae2991c3d3a
André Neves <andreneves3@gmail.com>
2022-05-09 01:23:34 -0300

    feat: main webapp layout structure

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,15 +4,32 @@ import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
+import { LayoutModule } from '@angular/cdk/layout';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 describe('AppComponent', () => {
-  let fixture: ComponentFixture<AppComponent>;
   let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
   let debugElement: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
       declarations: [AppComponent],
+      imports: [
+        RouterTestingModule,
+        NoopAnimationsModule,
+        LayoutModule,
+        MatButtonModule,
+        MatIconModule,
+        MatListModule,
+        MatSidenavModule,
+        MatToolbarModule,
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AppComponent);
@@ -24,17 +41,15 @@ describe('AppComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it(`should have as title 'scalio-challenge'`, () => {
-    expect(component.title).toEqual('scalio-challenge');
+  it(`should have as title 'GitHub Users Search'`, () => {
+    expect(component.title).toEqual('GitHub Users Search');
   });
 
   it('should render title', () => {
     fixture.detectChanges();
-    const title = debugElement.query(By.css('.content span'));
+    const title = debugElement.query(By.css('mat-toolbar span'));
     const titleElement = title.nativeElement as HTMLElement;
 
-    expect(titleElement.textContent).toContain(
-      'scalio-challenge app is running!'
-    );
+    expect(titleElement.textContent).toContain('GitHub Users Search');
   });
 });
