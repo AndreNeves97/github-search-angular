84603be66c41e591d21956f2dfa8f59cc9b66a23
André Neves <andreneves3@gmail.com>
2022-05-15 01:03:05 -0300

    test(progress bar): create new test cases

--- src/app/search/presenter/pages/github-search/components/atoms/progress-bar/progress-bar.component.spec.ts
+++ src/app/search/presenter/pages/github-search/components/atoms/progress-bar/progress-bar.component.spec.ts
@@ -1,25 +1,44 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { ProgressBarComponent } from './progress-bar.component';
 
 describe('ProgressBarComponent', () => {
   let component: ProgressBarComponent;
   let fixture: ComponentFixture<ProgressBarComponent>;
+  let debugElement: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProgressBarComponent ]
-    })
-    .compileComponents();
+      declarations: [ProgressBarComponent],
+    }).compileComponents();
   });
 
-  beforeEach(() => {
+  beforeEach(waitForAsync(() => {
     fixture = TestBed.createComponent(ProgressBarComponent);
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
     fixture.detectChanges();
-  });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show progress bar', () => {
+    component.isLoading = true;
+    fixture.detectChanges();
+
+    const progressBar = debugElement.query(By.css('mat-progress-bar'));
+    expect(progressBar?.nativeElement).toBeDefined();
+  });
+
+  it('should hide progress bar', () => {
+    component.isLoading = false;
+    fixture.detectChanges();
+
+    const progressBar = debugElement.query(By.css('mat-progress-bar'));
+    expect(progressBar?.nativeElement).toBeUndefined();
   });
 });
